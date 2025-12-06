882cf37c3369de207ede83fa4d671230da13683a
Felipe <ah193971@gmail.com>
2025-12-06 11:18:44 -0600

    Temperature converter added

--- main.go
+++ main.go
@@ -51,9 +51,9 @@ func showResult(d Data) {
 	fmt.Printf("%.2f %s %.2f = %.2f\n", d.num1, d.op, d.num2, d.result)
 }
 
-func main() {
-
+func calculations() {
 	var inputData Data
+
 	inputData.num1 = getFloatInput("Insert the first number: ")
 	inputData.op = getCharInput(" What operation you need? (+, -, /, *)")
 	inputData.num2 = getFloatInput("Write the second number: ")
@@ -83,4 +83,57 @@ func main() {
 	}
 
 	showResult(inputData)
+}
+
+func temp_converter() {
+	tempIn := getFloatInput("Insert the temperature you have: ")
+	unitIn := getCharInput("Insert the unit of the temperature (F, C, K)")
+	var tempOut float64
+	var unitOut string
+
+	switch unitIn {
+	case "F", "f":
+		tempOut = (tempIn - 32.0) * 5.0 / 9.0
+		unitOut = "C"
+
+	case "C", "c":
+		tempOut = (tempIn * 9.0 / 5.0) + 32.0
+		unitOut = "F"
+
+	case "K", "k":
+		tempOut = tempIn - 273.15
+		unitOut = "F"
+
+	default:
+		fmt.Println("Sorry, wrong input please use (F, C or K)")
+	}
+
+	fmt.Printf("Converted temperature: %.2f %s\n", tempOut, unitOut)
+}
+
+func main() {
+
+	for {
+		fmt.Println("What you want to do")
+		fmt.Println("1- To use calculator")
+		fmt.Println("2- To use a temperature converter")
+		fmt.Println("3- To exit")
+
+		menu := int(getFloatInput("Insert what you want to do: "))
+
+		switch menu {
+		case 1:
+			calculations()
+
+		case 2:
+			temp_converter()
+
+		case 3:
+			fmt.Println("Goodbye!")
+			return
+
+		default:
+			fmt.Println("Wrong input, please try again!")
+		}
+	}
 }
