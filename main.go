ccaecdd1d41ed40aa698f32a84535ac1e11230ef
Felipe <ah193971@gmail.com>
2025-12-08 22:58:02 -0600

    Adding history into calculations

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
+var history []Data
+
 type Data struct {
-	num1   float64
-	op     string
-	num2   float64
-	result float64
+	Num1   float64 `json:"num1"`
+	Op     string  `json:"op"`
+	Num2   float64 `json:"num2"`
+	Result float64 `json:"result"`
 }
 
 func getFloatInput(prompt string, reader *bufio.Reader) float64 {
@@ -22,7 +25,7 @@ func getFloatInput(prompt string, reader *bufio.Reader) float64 {
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
-		num, err := strconv.ParseFloat(input, 64) // If conversion succesful num save it, if not err will hold an error value
+		num, err := strconv.ParseFloat(input, 64) // If conversion successful num save it, if not err will hold an error value
 		if err == nil {                           // If err has no error value all good to go.
 			return num
 		} else {
@@ -48,41 +51,42 @@ func getCharInput(prompt string, reader *bufio.Reader) string {
 }
 
 func showResult(d Data) {
-	fmt.Printf("%.2f %s %.2f = %.2f\n", d.num1, d.op, d.num2, d.result)
+	fmt.Printf("%.2f %s %.2f = %.2f\n", d.Num1, d.Op, d.Num2, d.Result)
 }
 
 func calculations(reader *bufio.Reader) {
 	var inputData Data
 
-	inputData.num1 = getFloatInput("Insert the first number: ", reader)
-	inputData.op = getCharInput("What operation you need? (+, -, /, *)", reader)
-	inputData.num2 = getFloatInput("Write the second number: ", reader)
+	inputData.Num1 = getFloatInput("Insert the first number: ", reader)
+	inputData.Op = getCharInput("What operation you need? (+, -, /, *)", reader)
+	inputData.Num2 = getFloatInput("Write the second number: ", reader)
 
-	switch inputData.op {
+	switch inputData.Op {
 	case "+":
-		inputData.result = inputData.num1 + inputData.num2
+		inputData.Result = inputData.Num1 + inputData.Num2
 
 	case "-":
-		inputData.result = inputData.num1 - inputData.num2
+		inputData.Result = inputData.Num1 - inputData.Num2
 
 	case "*", "x", "X":
-		inputData.result = inputData.num1 * inputData.num2
+		inputData.Result = inputData.Num1 * inputData.Num2
 
 	case "/":
-		if inputData.num2 == 0 {
+		if inputData.Num2 == 0 {
 			fmt.Println("Sorry you can't divide by zero. ")
 			return
 
 		} else {
-			inputData.result = inputData.num1 / inputData.num2
+			inputData.Result = inputData.Num1 / inputData.Num2
 
 		}
 	default:
-		fmt.Printf("Error: invalid input '%s'\n", inputData.op)
+		fmt.Printf("Error: invalid input '%s'\n", inputData.Op)
 		return
 	}
 
 	showResult(inputData)
+	history = append(history, inputData)
 }
 
 func tempConverter(reader *bufio.Reader) {
@@ -112,6 +116,45 @@ func tempConverter(reader *bufio.Reader) {
 	fmt.Printf("Converted temperature: %.2f %s\n", tempOut, unitOut)
 }
 
+func saveHistory() {
+	file, err := os.Create("History.json")
+
+	if err != nil {
+		fmt.Println("Error creating file: ", err)
+		return
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t") // Adds identation for readability.
+	if err := encoder.Encode(history); err != nil {
+		fmt.Println("Error saving history: ", err)
+	} else {
+		fmt.Println("History save into History.json")
+	}
+}
+
+func showHistory() {
+	file, err := os.Open("History.json")
+	if err != nil {
+		fmt.Println("No history file found!")
+		return
+	}
+	defer file.Close()
+	var loadedHistory []Data
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&loadedHistory)
+
+	if err != nil {
+		fmt.Println("Error reading history:", err)
+		return
+	}
+	fmt.Println("--- History ---")
+	for _, d := range loadedHistory {
+		showResult(d)
+	}
+	fmt.Println("------------------")
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -119,7 +162,8 @@ func main() {
 		fmt.Println("What you want to do")
 		fmt.Println("1- To use calculator")
 		fmt.Println("2- To use a temperature converter")
-		fmt.Println("3- To exit")
+		fmt.Println("3- To show the history of calculations")
+		fmt.Println("4- To exit")
 
 		menu := int(getFloatInput("Insert what you want to do: ", reader))
 
@@ -131,6 +175,10 @@ func main() {
 			tempConverter(reader)
 
 		case 3:
+			showHistory()
+
+		case 4:
+			saveHistory()
 			fmt.Println("Goodbye!")
 			return
 
